fe2676fd81667a9e69dd43d2f02118ddb6525ba0
Skull <86374920+vSkull@users.noreply.github.com>
2022-02-02 23:56:25 +0200

    harness setup

---
+++ typescript/src/client/controllers/systems/harness.ts
@@ -0,0 +1,19 @@
+/*
+    just an sample for return values for some Harness functions | todo: make it decide it self..
+*/
+
+export function SetHarness() {
+    return false;
+}
+
+export function SetSeatBelt() {
+    return false;
+}
+
+export function HasSeatBeltOn() {
+    return false;
+}
+
+export function HasHarnessOn() {
+    return false;
+}
